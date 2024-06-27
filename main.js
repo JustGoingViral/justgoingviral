b6ad20a8d4d7fbdb237e25182f4e13e0cfba27e9
just-goingviral <admin@justgoingviral.com>
2024-06-27 15:31:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+/*
+
+  Just Going Viral © 2024
+  Express Server (Middle Man API)
+  
+  -- Main Program --
+
+    Author: Lancey Fem Denise Cruz
+    lancey@justgoingviral.com
+
+    This Express server provides a robust and flexible foundation for building web applications and APIs. 
+    Leveraging the power of Node.js and the simplicity of Express, this server is designed to handle basic 
+    routing for various HTTP methods, including GET, POST, PUT, and DELETE. It comes pre-configured with 
+    essential middleware for logging requests, parsing JSON payloads, and managing Cross-Origin Resource 
+    Sharing (CORS), ensuring smooth and secure data handling. The server also includes comprehensive error 
+    handling mechanisms to gracefully manage unexpected issues and maintain stability.
+    
+    In addition to its core functionalities, the server is set up to serve static files, making it suitable 
+    for hosting both dynamic and static content. With a clean and organized project structure, this Express 
+    server is easy to extend and customize according to your specific requirements. Whether you're building 
+    a simple API or a full-fledged web application, this server offers a reliable starting point, allowing 
+    you to focus on developing features and delivering value to your users.
+
+*/
+
+
 const goHighLevelToken = ''; //gohighleveltoken
 const nextechToken = ''; //nextechtoken
 
